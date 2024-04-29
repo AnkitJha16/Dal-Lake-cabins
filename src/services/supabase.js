e4cd3d5d1d18fb4089b8b861d60fbdcf1445ad1b
Ankit <ankit123@gmail.com>
2024-04-29 22:18:06 +0530

    modified Supabase.js

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,7 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 export const supabaseUrl = "https://aabjrxzrquhtfaqunzbe.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFhYmpyeHpycXVodGZhcXVuemJlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTI5OTExNjgsImV4cCI6MjAyODU2NzE2OH0.tS52nEnyUg7wjD9i0LYYrU0k0CqBWmH1aDM7FLaVp-s";
+const supabaseKey = import.meta.env.VITE_SUPA_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
